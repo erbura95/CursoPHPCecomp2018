578a0b0227ae72f72a6bfff88c0465d10af43a54
Marco Estrada Lopez <fericell2909@gmail.com>
2018-10-28 03:14:14 -0500

    Archivos de la Clase Seis

---
+++ CLASE_SEIS/index.php
@@ -0,0 +1,72 @@
+<?php
+require_once 'model/database.php';
+
+//echo $HTTP_SERVER_VARS["REQUEST_METHOD"];
+session_start();
+
+
+if (isset($_REQUEST['c']) == 'Login' && isset($_REQUEST['a']) == 'IniciarSesion') {
+    
+
+    // Obtenemos el controlador que queremos cargar
+        $controller = strtolower($_REQUEST['c']);
+        $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+        
+        // Instanciamos el controlador
+        require_once "controller/$controller.controller.php";
+        $controller = ucwords($controller) . 'Controller';
+        $controller = new $controller;
+        
+        // Llama la accion
+        call_user_func( array( $controller, $accion ) );
+
+}  else
+
+{
+
+    if(!isset($_SESSION["user"]) || empty($_SESSION["user"])){
+
+        $controller = 'login';
+
+        require_once "controller/$controller.controller.php";
+        $controller = ucwords($controller) . 'Controller';
+        $controller = new $controller;
+        $controller->Index(); 
+
+    } else
+    {
+        
+        $controller = 'inicio';
+
+        // Todo esta lógica hara el papel de un FrontController
+        if(!isset($_REQUEST['c']))
+        {
+            require_once "controller/$controller.controller.php";
+            $controller = ucwords($controller) . 'Controller';
+            $controller = new $controller;
+            $controller->Index();    
+        }
+        else
+        {
+            // Obtenemos el controlador que queremos cargar
+            $controller = strtolower($_REQUEST['c']);
+            $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+            
+
+            // Instanciamos el controlador
+            require_once "controller/$controller.controller.php";
+            $controller = ucwords($controller) . 'Controller';
+            $controller = new $controller;
+            
+
+            // Llama la accion
+            call_user_func( array( $controller, $accion ) );
+        }
+
+    }
+
+}
+
+
+
+
