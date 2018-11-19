135dbadb024076e5045737b2b02a028fff2b15e8
Marco Estrada Lopez <fericell2909@gmail.com>
2018-11-19 12:36:51 -0500

    Codigo realizado en la sesion 09

---
+++ CLASE_NUEVE/mvcapp9/view/footer.php
@@ -0,0 +1,13 @@
+            <div class="row">
+                <div class="col-xs-12">
+                                   
+				</div>    
+            </div>
+        </div>
+        <script src="../assets/js/jquery-2.2.3.min.js"></script>
+        <script src="../assets/js/bootstrap.min.js"></script>
+		<script src="../assets/js/jquery.bootgrid.min.js"></script>        
+        <script src="../assets/js/app.min.js"></script>
+        <script src="../assets/js/aplicacion.js"></script>
+</body>
+</html>
---
+++ CLASE_NUEVE/mvcapp9/view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title>Sistema de Biblioteca</title>
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        
+        <!--<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>-->
+       <!--<script src="assets/js/jquery-1.11.2.min.js"></script>-->
+    </head>
+    <body>
+        
+    <div class="container">
